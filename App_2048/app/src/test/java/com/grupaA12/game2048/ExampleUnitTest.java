91c689e84be819c9778b106b937e8be9d15d179f
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-05-16 21:39:03 +0200

    change junit to 5

--- App_2048/app/src/test/java/com/grupaA12/game2048/ExampleUnitTest.java
+++ App_2048/app/src/test/java/com/grupaA12/game2048/ExampleUnitTest.java
@@ -1,8 +1,7 @@
 package com.grupaA12.game2048;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).

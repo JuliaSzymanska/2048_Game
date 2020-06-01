ed579617980e1424c2e7792f502f9e0414f5f126
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-06-01 12:55:55 +0200

    Added new activity

---
+++ 2048_APP/app/src/main/java/com/game/a2048_app/BoardActivity.java
@@ -0,0 +1,17 @@
+package com.game.a2048_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+    }
+
+
+
+}
--- 2048_APP/app/src/main/java/com/game/a2048_app/MainActivity.java
+++ 2048_APP/app/src/main/java/com/game/a2048_app/MainActivity.java
@@ -1,7 +1,9 @@
 package com.game.a2048_app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configureStartGameButton();
+    }
+
+    private void configureStartGameButton(){
+        Button startGame = (Button) findViewById(R.id.startGameButton);
+        startGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, BoardActivity.class));
+            }
+        });
     }
 }

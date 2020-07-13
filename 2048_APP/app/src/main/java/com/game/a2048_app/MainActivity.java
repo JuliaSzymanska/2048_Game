95121b41b22802facbda7ec1534691652192bf40
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-07-13 16:55:18 +0200

    Rename

--- 2048_APP/app/src/main/java/com/game/a2048_app/MainActivity.java
+++ 2048_APP/app/src/main/java/com/game/a2048_app/MainActivity.java
@@ -1,14 +1,14 @@
 
 package com.game.a2048_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.game.module.Game;
 
@@ -60,8 +60,8 @@ public class MainActivity extends AppCompatActivity implements FingerprintDialog
     }
 
     private void configureAuthenticateButton() {
-        Button startGame = (Button) findViewById(R.id.authenticateButton);
-        startGame.setOnClickListener(authentication);
+        Button authenticationButton = (Button) findViewById(R.id.authenticateButton);
+        authenticationButton.setOnClickListener(authentication);
     }
 
 

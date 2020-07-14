5f352d57fc690c7384f8ab809250072c65586909
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-07-14 09:49:10 +0200

    Settings

--- 2048_APP/app/src/main/java/com/game/a2048_app/MainActivity.java
+++ 2048_APP/app/src/main/java/com/game/a2048_app/MainActivity.java
@@ -2,6 +2,8 @@
 package com.game.a2048_app;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
@@ -12,11 +14,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.game.module.Game;
 
+import java.util.Arrays;
+import java.util.List;
+
 import me.aflak.libraries.callback.FingerprintDialogCallback;
 import me.aflak.libraries.dialog.FingerprintDialog;
 
 
-public class MainActivity extends AppCompatActivity implements FingerprintDialogCallback{
+public class MainActivity extends AppCompatActivity implements FingerprintDialogCallback {
 
     MainActivity mainActivity = this;
 
@@ -32,6 +37,7 @@ public class MainActivity extends AppCompatActivity implements FingerprintDialog
     private void initButtons() {
         configureStartGameButton();
         configureAuthenticateButton();
+        configureSettingsButton();
     }
 
     private View.OnClickListener authentication = new View.OnClickListener() {
@@ -54,17 +60,55 @@ public class MainActivity extends AppCompatActivity implements FingerprintDialog
         }
     };
 
-    private void configureStartGameButton(){
+    private View.OnClickListener settings = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            String[] sensors = new String[]{
+                    "Light sensor",
+                    "Proximity sensor",
+                    "Magnetometer",
+                    "Accelerometer + Gyroscope"
+            };
+            final List<String> sensorsList = Arrays.asList(sensors);
+            final boolean[] chosenSensors = new boolean[]{false, false, false, false};
+
+            builder.setMultiChoiceItems(sensors, chosenSensors, new DialogInterface.OnMultiChoiceClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                    chosenSensors[which] = isChecked;
+                    String currentItem = sensorsList.get(which);
+
+                }
+            });
+            builder.setCancelable(false);
+            builder.setTitle("Which sensors to enable?");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+    };
+
+    private void configureStartGameButton() {
         Button startGame = (Button) findViewById(R.id.startGameButton);
         startGame.setOnClickListener(initializeBoardActivity);
     }
 
     private void configureAuthenticateButton() {
         Button authenticationButton = (Button) findViewById(R.id.authenticateButton);
-        authenticationButton.setOnClickListener(authentication);
         authenticationButton.setBackgroundResource(R.drawable.fingerprint_without_background);
+        authenticationButton.setOnClickListener(authentication);
     }
 
+    private void configureSettingsButton() {
+        Button settingsButton = (Button) findViewById(R.id.settingsButton);
+        settingsButton.setBackgroundResource(R.drawable.settings);
+        settingsButton.setOnClickListener(settings);
+    }
 
     @Override
     public void onAuthenticationSucceeded() {
@@ -73,7 +117,9 @@ public class MainActivity extends AppCompatActivity implements FingerprintDialog
 
     @Override
     public void onAuthenticationCancel() {
-    // FIXME: 11.07.2020
+        // FIXME: 11.07.2020
     }
+
+
 }
 

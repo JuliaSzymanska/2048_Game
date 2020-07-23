afb5335b171059efda876806828f1c968f5c0391
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-07-23 21:46:42 +0200

    Fade in/out

--- 2048_APP/app/src/main/java/com/game/a2048_app/SplashActivity.java
+++ 2048_APP/app/src/main/java/com/game/a2048_app/SplashActivity.java
@@ -2,6 +2,7 @@ package com.game.a2048_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,8 +11,18 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+
+        int SPLASH_TIME_OUT = 1000;
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
     }
 }

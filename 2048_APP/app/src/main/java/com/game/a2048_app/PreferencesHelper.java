4075084e9955319305ea684a724903a3be76ed3d
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-07-27 00:20:42 +0200

    Preferences Helper class begginging

--- 2048_APP/app/src/main/java/com/game/a2048_app/PreferencesHelper.java
+++ 2048_APP/app/src/main/java/com/game/a2048_app/PreferencesHelper.java
@@ -1,10 +1,15 @@
 package com.game.a2048_app;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 
 public class PreferencesHelper {
 
     private static final PreferencesHelper INSTANCE = new PreferencesHelper();
+
+    private static Context context;
+
+    private static SharedPreferences sharedPreferencesSettings;
 
     private PreferencesHelper() {
     }
@@ -13,9 +18,21 @@ public class PreferencesHelper {
         return INSTANCE;
     }
 
-    public boolean getDarkTheme(SharedPreferences preferences){
-//        return preferences.getBoolean(getResources().getString(R.string.dark_theme), false);
-        return true;
+    public static void initContext(Context context) {
+        if (PreferencesHelper.context == null) {
+            PreferencesHelper.context = context.getApplicationContext();
+            PreferencesHelper.sharedPreferencesSettings
+                    = PreferencesHelper.context.getSharedPreferences
+                    (PreferencesHelper.context.getResources().getString(R.string.settings), Context.MODE_PRIVATE);
+        }
+    }
+
+    public boolean getDarkTheme(){
+        if (PreferencesHelper.context == null) {
+            throw new NullPointerException(" no xd byczq");
+        }
+        return PreferencesHelper.sharedPreferencesSettings.
+                getBoolean(PreferencesHelper.context.getResources().getString(R.string.dark_theme), false);
     }
 
 }

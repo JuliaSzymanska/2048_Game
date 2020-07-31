135cd6edbf3f241325bc31a6c2d4aa118cde963b
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-07-31 22:49:10 +0200

    Deletions

--- 2048_APP/app/src/main/java/com/game/a2048_app/helpers/GenericHelper.java
+++
@@ -1,9 +0,0 @@
-package com.game.a2048_app.helpers;
-
-import android.os.Build;
-
-public class GenericHelper {
-    public static boolean canCancelAnimation() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
-    }
-}

a308871f12c7b5630074689f55589075603d63f9
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-07-31 19:13:02 +0200

    Multi threating

--- 2048_APP/app/src/main/java/com/game/a2048_app/SplashActivity.java
+++ 2048_APP/app/src/main/java/com/game/a2048_app/SplashActivity.java
@@ -13,20 +13,30 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Preloader preloader = Preloader.getInstance();
-        Preloader.initContext(this);
-        preloader.loadAssets();
+        Thread loadImagesThread = new Thread() {
+            @Override
+            public void run() {
+                Preloader preloader = Preloader.getInstance();
+                Preloader.initContext(SplashActivity.this);
+                preloader.loadAssets();
+
+            }
+
+        };
+        loadImagesThread.start();
+        while(loadImagesThread.isAlive()){
+        }
         int SPLASH_TIME_OUT = 1000;
         new Handler().postDelayed(new Runnable() {
 
-            @Override
-            public void run() {
-                Intent i = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(i);
-                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+                    @Override
+                    public void run() {
+                        Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                        startActivity(i);
+                        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                        finish();
+                    }
+                }, SPLASH_TIME_OUT);
 
     }
 }

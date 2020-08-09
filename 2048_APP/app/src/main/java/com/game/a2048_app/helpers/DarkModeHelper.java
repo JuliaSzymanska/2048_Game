7a2ceb96ab3750fd5f99ef0882b2de22ad066999
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-08-09 23:52:16 +0200

    javadoc

--- 2048_APP/app/src/main/java/com/game/a2048_app/helpers/DarkModeHelper.java
+++ 2048_APP/app/src/main/java/com/game/a2048_app/helpers/DarkModeHelper.java
@@ -7,7 +7,8 @@ import android.widget.ImageView;
 public class DarkModeHelper {
 
     /**
-     * Set dark or light theme.
+     * Set dark or light theme. Intended for use in Activities that implement darkMode using the DarkThemeView.
+     * @param darkThemeView View to be set to either to null or to {@link Preloader#getDarkThemeOn()}.
      */
     public static void setTheme(ImageView darkThemeView) {
         if (PreferencesHelper.getInstance().getDarkTheme()) {

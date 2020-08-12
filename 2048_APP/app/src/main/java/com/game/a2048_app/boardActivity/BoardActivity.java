fca855ef956213086054ce128ebe494267f9ab8f
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-08-12 20:48:23 +0200

    Dialog custom padding

--- 2048_APP/app/src/main/java/com/game/a2048_app/boardActivity/BoardActivity.java
+++ 2048_APP/app/src/main/java/com/game/a2048_app/boardActivity/BoardActivity.java
@@ -2,10 +2,8 @@ package com.game.a2048_app.boardActivity;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.drawable.Drawable;

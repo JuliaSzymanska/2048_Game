a785e58cdebf385058903d49b5dcaafc1aaf9134
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-08-19 20:43:53 +0200

    Stop game

--- 2048_APP/app/src/main/java/com/game/a2048_app/boardActivity/Sensors/StopGameProximity.java
+++ 2048_APP/app/src/main/java/com/game/a2048_app/boardActivity/Sensors/StopGameProximity.java
@@ -18,20 +18,28 @@ public class StopGameProximity implements Runnable, SensorEventListener {
 
     private final static int PROXIMITY_DISTANCE = 5;
 
+    private float mProximityData;
+    private Game game;
+    private Context context;
+    private BoardActivityListener boardActivityListener;
+
+    /**
+     * Class constructor.
+     * @param context context from activity.
+     * @param game current game.
+     */
     public StopGameProximity(Context context, Game game) {
         this.context = context;
         this.game = game;
         this.boardActivityListener = (BoardActivityListener) context;
     }
 
-    private float mProximityData;
-    private Game game;
-    private Context context;
-    private BoardActivityListener boardActivityListener;
-
+    /**
+     * {@inheritDoc}
+     * Pauses or unpauses game according to {@link StopGameProximity#mProximityData}.
+     */
     @Override
     public void run() {
-        // Proximity sensor - zatrzymuje sie czas po zblizeniu
         if (mProximityData < PROXIMITY_DISTANCE) {
             if (!game.isSuspended()) {
                 boardActivityListener.callback(context.getString(R.string.SetPauseOn));
@@ -41,6 +49,10 @@ public class StopGameProximity implements Runnable, SensorEventListener {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     * Starts new thread to pause or unpause game.
+     */
     @Override
     public void onSensorChanged(SensorEvent event) {
         mProximityData = event.values[0];
@@ -49,6 +61,9 @@ public class StopGameProximity implements Runnable, SensorEventListener {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 

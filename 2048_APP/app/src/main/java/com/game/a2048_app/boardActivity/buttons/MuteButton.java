a8c7ac7dab9b2068f1b99cd7be737fe4ecae1efd
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-08-19 20:21:28 +0200

    Mute button

--- 2048_APP/app/src/main/java/com/game/a2048_app/boardActivity/buttons/MuteButton.java
+++ 2048_APP/app/src/main/java/com/game/a2048_app/boardActivity/buttons/MuteButton.java
@@ -8,17 +8,23 @@ import com.game.a2048_app.helpers.PreferencesHelper;
 import com.game.a2048_app.helpers.Preloader;
 import com.game.a2048_app.helpers.SoundPlayer;
 
-
-// TODO: 09.08.2020 dodać xml do przycisku
 public class MuteButton extends androidx.appcompat.widget.AppCompatButton {
-
-    // TODO: 09.08.2020 Wyrzuciłem tutaj ten mute button, ale jako że nie jestem znawcą androida to kurna nwm czy tak powinno być xDD
 
     private final PreferencesHelper preferencesHelper = PreferencesHelper.getInstance();
     private Preloader preloader = Preloader.getInstance();
 
     public MuteButton(Context context) {
         super(context);
+        this.prepareButton();
+    }
+
+    public MuteButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.prepareButton();
+    }
+
+    public MuteButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
         this.prepareButton();
     }
 
@@ -34,19 +40,14 @@ public class MuteButton extends androidx.appcompat.widget.AppCompatButton {
         }
     };
 
+    /**
+     * Calls methods:
+     * {@link MuteButton#setMuteButtonImage()} to set buttons image,
+     * {@link MuteButton#setOnClickListener(OnClickListener)} to set on click listener.
+     */
     private void prepareButton() {
         this.setMuteButtonImage();
         this.setOnClickListener(this.onClickListener);
-    }
-
-    public MuteButton(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        this.prepareButton();
-    }
-
-    public MuteButton(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        this.prepareButton();
     }
 
     /**
@@ -75,7 +76,6 @@ public class MuteButton extends androidx.appcompat.widget.AppCompatButton {
      * Creates button on click listener to mute or unmute application.
      * Play sound after click and change button's image.
      */
-    // TODO: 09.08.2020 chce to usstawić w xml
     public void muteButtonOnClick(View v) {
         if (preferencesHelper.getVolume() == 0) {
             preferencesHelper.setVolume(1);

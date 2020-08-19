3f41790c0b7cd2b82b4ffa99171487207e71aafa
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-08-19 21:17:32 +0200

    Credits

--- 2048_APP/app/src/main/java/com/game/a2048_app/credits/Credits.java
+++ 2048_APP/app/src/main/java/com/game/a2048_app/credits/Credits.java
@@ -16,6 +16,9 @@ public class Credits extends AppCompatActivity {
     private final static String PRZEMEK_GITHUB = "https://github.com/ZdrzalikPrzemyslaw";
     private final static String JULIA_GITHUB = "https://github.com/JuliaSzymanska";
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,12 +27,20 @@ public class Credits extends AppCompatActivity {
         this.loadData();
     }
 
+    /**
+     * On click listener for Przemek's text view.
+     * @param v view.
+     */
     public void onClickTextViewPrzemek(View v) {
         Intent i = new Intent(Intent.ACTION_VIEW);
         i.setData(Uri.parse(PRZEMEK_GITHUB));
         startActivity(i);
     }
 
+    /**
+     * On click listener for Julia's text view.
+     * @param v view.
+     */
     public void onClickTextViewJulia(View v) {
         Intent i = new Intent(Intent.ACTION_VIEW);
         i.setData(Uri.parse(JULIA_GITHUB));
